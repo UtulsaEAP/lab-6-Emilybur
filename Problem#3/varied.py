bff12bd21ae9e0b11e51a33a1b603a22651f1ce1
Emily Burke <erb9078@utulsa.edu>
2024-03-08 16:04:54 -0600

    finished problem 3

--- Problem#3/varied.py
+++ Problem#3/varied.py
@@ -1,20 +1,22 @@
 def process_input(input_string):
-      # Split into separate strings
+    # Split into separate strings
+    #Emily Burke Lab Friday at 3
+    numbers = input_string.split()
 
     # Convert strings to floats
-    
+    numbers = [float(num) for num in numbers]
 
     # Get max and average
-    max_value = 
-    average_value = 
+    max_value = max(numbers)
+    average_value = sum(numbers) / len(numbers)
     return max_value, average_value
 
 if __name__ == "__main__":
-    # User inputs string w/ numbers
-    user_input = input("Enter a space-separated string of numbers: ")
-
+    
     # Call the function and get the results
+    user_input=input()
     max_value, average_value = process_input(user_input)
 
-    print(f'Max Value: {max_value:.2f}')
-    print(f'Average Value: {average_value:.2f}')
+    print(f'{max_value:.2f}, {average_value:.2f}')
+    
+

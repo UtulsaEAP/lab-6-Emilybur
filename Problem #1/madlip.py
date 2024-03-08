aa6bec984b147616208fe073591a78859644905a
Emily Burke <erb9078@utulsa.edu>
2024-03-08 15:59:20 -0600

    finished problem 2

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,9 +1,17 @@
 def food_input():
-    user_input = input()
-    tokens = user_input.split()
     #type you while  loop here
-
+    while True: 
+        user_input = input()
+        tokens = user_input.split()
+        
+        if tokens[0] == 'quit':
+            break
+        
+        word = tokens[0]
+        number = tokens[1]
+        
+        output = f"Eating {number} {word} a day keeps you happy and healthy."
+        print(output)
     
-
-if __name__ == "__main__":
-    food_input()
+    if __name__ == "__main__":
+        food_input()
--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,15 @@
 def check_palindrome(user_input):
- #type your code here
+    # Remove spaces from input
+    #Emily Burke Lab Friday at 3
+    user_input = user_input.replace(" ", "")
+    
+    # Check if the input is equal to its reverse
+    if user_input == user_input[::-1]:
+        print(f"palindrome: {user_input}")
+    else:
+        print(f"not a palindrome: {user_input}")
+
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
+

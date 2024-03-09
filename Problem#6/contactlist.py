0a0602ae06957ec1aa1ab839212545e2de8bdcd7
Emily Burke <erb9078@utulsa.edu>
2024-03-08 18:08:48 -0600

    Lab 6

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,16 +1,14 @@
 def process_user_contacts(user_input):
-    user_contacts = 
+    user_contacts = {}
+    tokens = user_input.split()
+    for i in range(0, len(tokens), 2):
+        name = tokens[i]
+        phone_number = tokens[i + 1]
+        user_contacts[name] = phone_number
 
- 
-    user_input = 
-    tokens = 
-
-    # Put word pairs into a dictionary
-    
-    # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
-    
-   
+    print(user_contacts[contact_name])
+
 if __name__ == '__main__':
     # Get input for word pairs
     user_input = input("Enter word pairs (name, phone number): ")
